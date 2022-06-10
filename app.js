6415a463a850789dafb4e5dcdaee34e1933093ca
LauDAlvarez <lautarodalvarez@gmail.com>
2022-06-10 18:06:21 -0300

    Aplicamos express y levantamos servidor

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+
+app.listen(3000, console.log('Corriendo...'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
